1039d602618e582e29a07a87dba9d7f365316e73
zohaibmahmood151 <zohaibkasana151@gmail.com>
2026-06-17 13:53:14 +0500

    Add files via upload

---
+++ about.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Home Page</title>
+ <link rel="stylesheet" href="css/bootstrap.min.css" >
+ <link rel="stylesheet" href="css/bootstrap-theme.min.css" >
+ <link rel="stylesheet" href="css/custom.css" >
+ <script src="js/jquery.js"></script>
+ <script src="js/bootstrap.js" ></script>
+ <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+ </head>
+ <body>
+ <nav class="navbar navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed btn-clr" data-toggle="collapse" data-target="#abc" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar bar-clr"></span>
+        <span class="icon-bar bar-clr"></span>
+        <span class="icon-bar bar-clr"></span>
+      </button>
+	  <img src="img/logo.jpg" class="logo" style="width:10%;">
+      <a class="navbar-brand" href="#"></a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="abc">
+      
+   
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home</a></li>
+		 
+             
+	       </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<div class="container-fluid"  >
+	<div class="row " >
+	   <div class="col-lg-6">
+	<br><br><br><h2 class= "aboutplan">ABOUT US</h2>
+	<h3 class="aboutplan">Our philosophy.</h3>
+	<p class="with">Event Planner Ltd is an event logistics and marketing company which was formed back in 2013. The company offers A-Z event planning services from a team of experienced and energetic event planners, suppliers, venues and more. One of the main reasons behind the success of Event Planner is the fact that the team does not charge fees to its clients! With the number of events we organise, Event Planner Ltd does not need to add exorbitant fees and mark-ups to make its profit margins. This ensures that our clients list, which is constantly growing, make regular use of our services.</p>
+		<h3 class="aboutplan">There is no fee. There is no mark-up!</h3>
+			<p class="with">The Event Planner team does not charge any fees to its clients*. Yes, this might sound strange. However, the company earns its profits through its suppliers, with whom a very strong relationship has been built. This does not mean that the costs are up-marked to make up for the fee. On the contrary, they are less than it would cost a company when booking directly. We know it sounds too good to believe, but over the years this formula has helped the Event Planner team become the success story they are today. This has not in any way reduced the level of professionalism with which the services are carried, as shown by the large number of local and international clients,  who work with us on a regular basis.
+
+In a nutshell, you pay no fee, pay your supplier less and have a professional team handling all your loose ends. Sounds too good, but it is true! You can check out our clients as testimonials</p>
+<h3 class="aboutplan">The Team</h3>
+<p class="with">The Event Planner team is made up of a mix of people. James and Ilona take care of corporate events. Josette is the wedding guru. Nicole is the creative one. Sergio is the merchandise expert. In addition to this group of people, a large support team helps out in everything the company does, in office work to on-site logistics planning. One thing we can truly say about our team is that we are very passionate about our job!</p>
+<h3 class="aboutplan">How can we help you organise a successful event?</h3>
+<p class="with">1.      A-Z event planning. We shall work on your event from beginning to end. You may relax and leave all the logistics to us.<br><br>
+
+2.      Venue Consultancy. Finding a venue in Malta is not as easy as people make it sound. While offering an extensive list of venues on our website, we know and have contact with hundreds of venues around the Maltese Islands, one of which will most definitely fit into your event scenario.<br><br>
+
+3.      We organise: product launches, gala receptions, seminars and conferences, staff events, awards nights, team building events, B2B and B2C networking events, weddings and more.<br><br>
+
+4.      We give you more: transport services, photography, videography, furniture rental, entertainment, sound and lighting, branding, security, RSVP communication and follow up, staff, sms/email marketing, handle VIPs, catering and any event-related consultancy.</p>
+
+	  </div>
+	
+	  <div class="col-lg-6">
+	  
+	  
+	  <div class="container-fluid sec1" >
+ 
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+<link rel="stylesheet" type="text/css" href="test.css">
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/5.jpg"  style="width:100%;  height:800px;">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/a.jpg"  style="width:100%; height:800px;">
+        <div class="carousel-caption">
+          
+          
+        </div>
+      </div>
+    
+      <div class="item">
+        <img src="img/1141.jpg" alt="New York" style="width:100%; height:800px;">
+        <div class="carousel-caption">
+          
+        </div>
+      </div>
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+	 </div>
+	 </div> 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 	</div>
+</div>
+</body>
+</html>
